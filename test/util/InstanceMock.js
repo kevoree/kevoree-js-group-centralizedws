a62cbcf2f948d1f603dd88e45c80c8fe2ca6aac4
Maxime Tricoire <max.tricoire@gmail.com>
2017-03-14 14:42:32 +0100

    v1.0.0-alpha.9: should improve heartbeat process

--- test/util/InstanceMock.js
+++ test/util/InstanceMock.js
@@ -5,9 +5,27 @@ function InstanceMock(nodeName, name) {
 	this.name = name;
 	this.currentModel = null;
 	this.kInstance = kGroupMock;
+	this.dictionary = {
+		getString: function (name) {
+			switch (name) {
+				case 'masterNet':
+					return 'lo.ipv4';
+				case 'onDisconnect':
+					return '// nothing';
+				default:
+					return undefined;
+			}
+		}
+	};
+	this.dic_onDisconnect = {};
+	this.dic_onDisconnect.defaultValue = 'lo.ipv4';
 }
 
 InstanceMock.prototype = {
+	getName: function () {
+		return this.name;
+	},
+
 	getNodeName: function () {
 		return this.nodeName;
 	},
@@ -20,12 +38,17 @@ InstanceMock.prototype = {
 			},
 			deploy: function () {},
 			on: function () {},
-			off: function () {}
+			off: function () {},
+			submitScript: function () {}
 		};
 	},
 
 	getModelEntity: function () {
 		return this.kInstance;
+	},
+
+	getDictionary: function () {
+		return this.dictionary;
 	}
 };
 
